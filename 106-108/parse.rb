709c06a249491f9af5c011a38bc771988fec75aa
Data Shinobi <datashinobi108@gmail.com>
2019-11-09 11:09:23 -0500

    106-108: Top countries by alcohol consumption
    - Also archived canada fed. election data parse

---
+++ 106-108/parse.rb
@@ -0,0 +1,52 @@
+require 'json'
+require 'awesome_print'
+require 'csv'
+
+years = JSON.parse(File.read("years.json"))
+headers = years.dup
+headers.unshift("url")
+headers.unshift("country")
+countries = JSON.parse(File.read("countries.json"))
+countryCodes = countries.keys
+
+megaMap = {}
+countryCodes.each do |country|
+  megaMap[country] = {}
+  years.each do |year|
+    megaMap[country][year] = ""
+  end
+end
+
+# ap megaMap
+File.open("data_108.csv").each do |line|
+  cleanLine = line.strip
+
+  arr = cleanLine.split(",")
+  countryCode = arr[1]
+  year = arr[2]
+  val = arr[3]
+
+  if megaMap[countryCode] != nil
+    megaMap[countryCode][year] = val.to_f * 1000
+  end
+end
+
+CSV($stdout, headers: headers, write_headers: true) do |out|
+  countries.each do |key, values|
+    out << CSV::Row.new(headers, []).tap do |row|
+      previousYear = nil
+      years.each do |year|
+        row["country"] = values["label"]
+        row["url"] = values["url"]
+        # row[year] = megaMap[key][year]
+        value = megaMap[key][year]
+        if value == "" && previousYear != nil
+          row[year] = previousYear
+        else
+          row[year] = megaMap[key][year].to_i
+          previousYear = row[year]
+        end
+      end
+    end
+  end
+end
--- README.md
+++ README.md
@@ -5,3 +5,9 @@ Parsers
 - 103: Top countries with the ost mobile subscriptions (1985-2017)
 - 104: Most Populated countries in the world (1800-2019)
 - 105: Top countries GDP per capita (1820-2016)
+- 106: Top Countries by Wine Consumption per person (1961-2014)
+- 107: Top Countries by Spirits Consumption per person (1961-2014)
+- 108: Top Countries by Beer Consumption per person (1961-2014)
+- 109: Future Population Growth by Country (2000-2100)
+- 110: Cryptocurreny prices over time
+- 111: Human Development Index by Country (1990-2017)
